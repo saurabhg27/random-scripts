52b2eb587fbad58d1d6c7596016909540bc566c3
Saurabh <saurabhgaggar27@gmail.com>
2020-05-20 19:28:09 +0530

    Add files via upload

---
+++ convert_black_to_red.sh
@@ -0,0 +1,13 @@
+# Convert a PDF with black text and images into red using ImageMagick
+# Helps if you want to save black printer ink
+# https://superuser.com/a/1548631/1130873
+
+echo "start"
+for filename in *.pdf; do
+	outfilename="${filename%.pdf} output.pdf"
+	echo "---- start $filename --> $outfilename ----"
+	magick.exe -density 300 "$filename" -fuzz 50% -fill red -opaque black "$outfilename"
+	echo "---- done $outfilename ----"
+done
+
+echo "finished"
